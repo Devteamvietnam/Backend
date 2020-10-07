1296decaa99e018fcd0fdbb2a3f0ef010a200443
Ivanluca <jungjihoonkr97@gmail.com>
2020-10-07 23:43:19 +0700

    integrated login with GG, FB

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,11 +1,14 @@
 const passport = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy
 const LocalStrategy = require('passport-local').Strategy
+const GooglePlusTokenStrategy = require('passport-google-token').Strategy
+const FacebookTokenStrategy = require('passport-facebook-token')
 const { ExtractJwt } = require('passport-jwt')
-const { JWT_SECRET } = require('../configs')
+const { JWT_SECRET, auth } = require('../configs')
 
 const User = require('../models/User')
 
+//passport jwt
 passport.use(new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken('Authorization'),
   secretOrKey: JWT_SECRET
@@ -39,3 +42,74 @@ passport.use(new LocalStrategy({
     done(error, false)
   }
 }))
+
+// Passport Google
+passport.use(
+  new GooglePlusTokenStrategy(
+    {
+      clientID: auth.google.CLIENT_ID,
+      clientSecret: auth.google.CLIENT_SECRET
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+         // check whether this current user exists in our database
+         const user = await User.findOne({
+          authGoogleID: profile.id,
+          authType: "google", // find again with authType
+        });
+        // 
+        if (user) return done(null, user)
+        // If new account
+        const newUser = new User({
+          authType: 'google',
+          authGoogleID: profile.id,
+          email: profile.emails[0].value,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName
+        })
+
+        await newUser.save()
+
+        done(null, newUser)
+      } catch (error) {
+        done(error, false);
+      }
+    }
+  )
+);
+
+// Passport Facebook
+passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: auth.facebook.CLIENT_ID,
+      clientSecret: auth.facebook.CLIENT_SECRET,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // check whether this current user exists in our database
+        const user = await User.findOne({
+          authFacebookID: profile.id,
+          authType: "google",
+        });
+
+        if (user) return done(null, user)
+
+        // If new account
+        const newUser = new User({
+          authType: 'facebook',
+          authFacebookID: profile.id,
+          email: profile.emails[0].value,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName
+        })
+
+        await newUser.save()
+
+        done(null, newUser)
+      } catch (error) {
+        done(error, false);
+      }
+    }
+  )
+);
